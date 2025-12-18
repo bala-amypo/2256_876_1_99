4ed904166f7be3de7f1538e5a64906a5f6f3db81
bala-amypo <bala@amypo.in>
2025-12-18 02:32:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -6,5 +6,82 @@ import java.time.LocalDate;
 
 @Entity
 public class Asset{
-    
+    @Id
+    private Long id;
+    private String assetTag;
+    private String assetName;
+    private ManyToOne vendor;
+    private LocalDate purchaseDate;
+    private double purchaseCost;
+    private ManyToOne depreciationRule;
+    private String status;
+    private LocalDateTime createAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getAssetTag() {
+        return assetTag;
+    }
+    public void setAssetTag(String assetTag) {
+        this.assetTag = assetTag;
+    }
+    public String getAssetName() {
+        return assetName;
+    }
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
+    }
+    public ManyToOne getVendor() {
+        return vendor;
+    }
+    public void setVendor(ManyToOne vendor) {
+        this.vendor = vendor;
+    }
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+    public double getPurchaseCost() {
+        return purchaseCost;
+    }
+    public void setPurchaseCost(double purchaseCost) {
+        this.purchaseCost = purchaseCost;
+    }
+    public ManyToOne getDepreciationRule() {
+        return depreciationRule;
+    }
+    public void setDepreciationRule(ManyToOne depreciationRule) {
+        this.depreciationRule = depreciationRule;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+    public Student(Long id, String assetTag, String assetName, ManyToOne vendor, LocalDate purchaseDate,
+            double purchaseCost, ManyToOne depreciationRule, String status, LocalDateTime createAt) {
+        this.id = id;
+        this.assetTag = assetTag;
+        this.assetName = assetName;
+        this.vendor = vendor;
+        this.purchaseDate = purchaseDate;
+        this.purchaseCost = purchaseCost;
+        this.depreciationRule = depreciationRule;
+        this.status = status;
+        this.createAt = createAt;
+    }
+    public Student() {
+    }
 }
---
+++ src/main/java/com/example/demo/entity/Vendor.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Vendor{
+    @Id
+    
+}
