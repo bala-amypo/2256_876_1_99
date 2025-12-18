05ddead108930587b97fc7ddf715f7aa88732ee9
bala-amypo <bala@amypo.in>
2025-12-18 02:12:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -5,6 +5,49 @@ import jakarta.persistence.*;
 @Entity
 public class User{
     @Id
-    
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime createAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+    public Student(Long id, String name, String email, String password, LocalDateTime createAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createAt = createAt;
+    }
+    public Student() {
+    }
 }
 
