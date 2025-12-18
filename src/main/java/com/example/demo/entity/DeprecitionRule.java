313b961538320e99d73ad3909898eaa6fe5985a9
bala-amypo <bala@amypo.in>
2025-12-18 02:37:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/DeprecitionRule.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class DeprecitionRule{
+    @Id
+    
+}
--- src/main/java/com/example/demo/entity/Vendor.java
+++ src/main/java/com/example/demo/entity/Vendor.java
@@ -6,5 +6,48 @@ import java.time.LocalDateTime;
 @Entity
 public class Vendor{
     @Id
-    
+    private Long  id;
+    private String vendorName;
+    private String contactEmail;
+    private String phone;
+    private LocalDateTime createAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getVendorName() {
+        return vendorName;
+    }
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+    public String getContactEmail() {
+        return contactEmail;
+    }
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+    public Student(Long id, String vendorName, String contactEmail, String phone, LocalDateTime createAt) {
+        this.id = id;
+        this.vendorName = vendorName;
+        this.contactEmail = contactEmail;
+        this.phone = phone;
+        this.createAt = createAt;
+    }
+    public Student() {
+    }
 }
