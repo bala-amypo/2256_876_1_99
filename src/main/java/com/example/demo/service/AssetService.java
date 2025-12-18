9cdd72f22d1d3adc717a30b3e1e7ce7ecf459c31
bala-amypo <bala@amypo.in>
2025-12-18 09:09:35 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetService.java
+++ src/main/java/com/example/demo/service/AssetService.java
@@ -3,4 +3,7 @@ package com.example.demo.service;
 import java.util.*;
 import com.example.demo.entity.Asset;
 
-public interface
+public interface AssetService{
+    public Asset createAsset(Long vendorld,Long ruleld,Asset asset);
+    
+}
