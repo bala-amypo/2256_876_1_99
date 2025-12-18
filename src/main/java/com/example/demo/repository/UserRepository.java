ed0a06723e1d6f8b8af5f6625cf1830e1aad6c36
bala-amypo <bala@amypo.in>
2025-12-18 15:44:07 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<StudentEntity,Integer>{
-        boolian  
+public interface UserRepository extends JpaRepository<User,Long>{
+        Optional<User>findByEmail(String email);
 }
