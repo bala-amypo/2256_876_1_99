4aec3740a639ea2632907cba6df7fa03cab7ca0d
bala-amypo <bala@amypo.in>
2025-12-18 02:27:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -1,8 +1,10 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import org.hibernate.mapping.ManyToOne;
+import java.time.LocalDate;
 
 @Entity
-
 public class Asset{
     
 }
