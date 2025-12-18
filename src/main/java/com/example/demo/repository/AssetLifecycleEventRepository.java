3728163346d5722646c1ed7464d04d0940bc2109
bala-amypo <bala@amypo.in>
2025-12-18 15:59:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/AssetLifecycleEventRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.AssetLifecycleEvent;
+
+@Repository
+public interface AssetLifecycleEventRepository extends JpaRepository<StudentEntity,Integer>{
+
+}
