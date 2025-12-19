6e5a496c49a233c2b4928fa903c960b430c25d91
bala-amypo <bala@amypo.in>
2025-12-19 04:44:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssetDisposalRepository.java
+++ src/main/java/com/example/demo/repository/AssetDisposalRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.AssetDisposal;
+import com.example.demo.entity.User;
+import java.util.*;
+
 
 @Repository
 public interface AssetDisposalRepository extends JpaRepository<AssetDisposal,Long>{
