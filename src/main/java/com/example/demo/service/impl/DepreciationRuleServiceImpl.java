a10c9689f7540c6ad12771038cf4d58fd3d614ed
bala-amypo <bala@amypo.in>
2025-12-20 10:21:36 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DepreciationRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DepreciationRuleServiceImpl.java
@@ -3,42 +3,42 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.*;
 import com.example.demo.repository.*;
 import com.example.demo.service.*;
+
 import org.springframework.stereotype.Service;
+
 import java.time.*;
 import java.util.*;
-
 
 @Service
 public class DepreciationRuleServiceImpl implements DepreciationRuleService {
 
-    private final DepreciationRuleRepository ruleRepository;
+    private final DepreciationRuleRepository depreciationRuleRepository;
 
-    public DepreciationRuleServiceImpl(DepreciationRuleRepository ruleRepository) {
-        this.ruleRepository = ruleRepository;
+    public DepreciationRuleServiceImpl(DepreciationRuleRepository depreciationRuleRepository) {
+        this.depreciationRuleRepository = depreciationRuleRepository;
     }
 
     @Override
     public DepreciationRule createRule(DepreciationRule rule) {
 
         if (rule.getUsefulLifeYears() <= 0) {
-            throw new IllegalArgumentException("Useful life must be greater than 0");
+            throw new IllegalArgumentException("usefulLifeYears must be > 0");
         }
 
         if (rule.getSalvageValue() < 0) {
-            throw new IllegalArgumentException("Salvage value cannot be negative");
+            throw new IllegalArgumentException("salvageValue must be >= 0");
         }
 
-        if (!rule.getMethod().equals("STRAIGHT_LINE") && !rule.getMethod().equals("DECLINING_BALANCE")) {
+        if (!rule.getMethod().equals("STRAIGHT_LINE") &&!rule.getMethod().equals("DECLINING_BALANCE")) {
             throw new IllegalArgumentException("Invalid depreciation method");
         }
 
         rule.setCreatedAt(LocalDateTime.now());
-        return ruleRepository.save(rule);
+        return depreciationRuleRepository.save(rule);
     }
 
     @Override
-        public List<DepreciationRule> getAllRules() {
+    public List<DepreciationRule> getAllRules() {
         return depreciationRuleRepository.findAll();
     }
-
 }
