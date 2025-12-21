24e269ac47f81c2fa7cecf1db52f9c09ad5f7488
bala-amypo <bala@amypo.in>
2025-12-21 16:43:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/DataUtil.java
@@ -0,0 +1,76 @@
+package com.example.demo.util;
+
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class DataUtil implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DataUtil(RoleRepository roleRepository,
+                    UserRepository userRepository,
+                    PasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        
+        if (roleRepository.findByName("ADMIN").isEmpty()) {
+            roleRepository.save(new Role("ADMIN"));
+        }
+
+        if (roleRepository.findByName("USER").isEmpty()) {
+            roleRepository.save(new Role("USER"));
+        }
+
+    
+        if (userRepository.findByEmail("admin@example.com").isEmpty()) {
+            Role adminRole = roleRepository.findByName("ADMIN").get();
+
+            Set<Role> roles = new HashSet<>();
+            roles.add(adminRole);
+
+            User admin = new User();
+            admin.setName("Admin User");
+            admin.setEmail("admin@example.com");
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setRoles(roles);
+            admin.setCreatedAt(LocalDateTime.now());
+
+            userRepository.save(admin);
+        }
+
+        
+        if (userRepository.findByEmail("user@example.com").isEmpty()) {
+            Role userRole = roleRepository.findByName("USER").get();
+
+            Set<Role> roles = new HashSet<>();
+            roles.add(userRole);
+
+            User user = new User();
+            user.setName("Normal User");
+            user.setEmail("user@example.com");
+            user.setPassword(passwordEncoder.encode("user123"));
+            user.setRoles(roles);
+            user.setCreatedAt(LocalDateTime.now());
+
+            userRepository.save(user);
+        }
+    }
+}
