3e3d4001ecd97bdda7ed3d29523cc47a9bbca981
bala-amypo <bala@amypo.in>
2025-12-21 11:26:38 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,35 +1,35 @@
-// package com.example.demo.dto;
+package com.example.demo.dto;
 
-// import java.util.Set;
+import java.util.Set;
 
-// public class AuthResponse {
+public class AuthResponse {
 
-    // private String token;
-    // private Long userId;
-    // private String email;
-    // private Set<String> roles;
+    private String token;
+    private Long userId;
+    private String email;
+    private Set<String> roles;
 
-    // public AuthResponse(String token, Long userId, String email, Set<String> roles) {
-    //     this.token = token;
-    //     this.userId = userId;
-    //     this.email = email;
-    //     this.roles = roles;
-    // }
+    public AuthResponse(String token, Long userId, String email, Set<String> roles) {
+        this.token = token;
+        this.userId = userId;
+        this.email = email;
+        this.roles = roles;
+    }
 
-    // public String getToken() {
-    //     return token;
-    // }
+    public String getToken() {
+        return token;
+    }
 
-    // public Long getUserId() {
-    //     return userId;
-    // }
+    public Long getUserId() {
+        return userId;
+    }
 
-    // public String getEmail() {
-    //     return email;
-    // }
+    public String getEmail() {
+        return email;
+    }
 
-    // public Set<String> getRoles() {
-    //     return roles;
-    // }
-// }
+    public Set<String> getRoles() {
+        return roles;
+    }
+}
 
